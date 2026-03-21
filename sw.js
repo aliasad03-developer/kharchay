853dbc7c4e161bc974bbb6de194104aa979f7df5
aliasad03-developer <aliasad2984@gmail.com>
2026-03-21 12:42:51 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'kharchay-v3';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) { cache.put(e.request, clone); });
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
